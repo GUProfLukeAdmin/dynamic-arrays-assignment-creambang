803a3ae3caffb62a26e507c35f0eff7eb9fbe111
Lan Anh Ha Thi <lananhhathi@Lans-MacBook-Air.local>
2025-02-12 20:40:51 -0800

    Submitting Assignment

--- vector.cpp
+++ vector.cpp
@@ -1,12 +1,30 @@
 #include <vector>
-
+#include<iostream>
 
 void part1() {
-    //TODO: Implement part 1
+    std::vector<int> myVec;
+    for(int i = 0; i < 50000; i++){
+        myVec.push_back(i);
+    }
+
+    std::cout << "After adding elements: Size = " << myVec.size() << ", Capacity = " << myVec.capacity() << std::endl;
+
+    myVec.resize(myVec.size()/2);
+    std::cout << "After resizing: Size = " << myVec.size() << ", Capacity = " << myVec.capacity() << std::endl;
+
+    myVec.shrink_to_fit();
+    std::cout << "After shrink_to_fit(): Size = " << myVec.size() << ", Capacity = " << myVec.capacity() << std::endl;
 }
 
 void part2() {
-    //TODO: Implement part 2
+    std::vector<bool> boolVec(10000);
+    std::vector<char> charVec(10000);
+
+    int boolMemory = boolVec.size()/8;
+    int charMemory = charVec.size();
+
+    std::cout << "Memory usage of std::vector<bool>: " << boolMemory << " bytes" << std::endl;
+    std::cout << "Memory usage of std::vector<char>: " << charMemory << " bytes" << std::endl;
 }
 
 int main() {
